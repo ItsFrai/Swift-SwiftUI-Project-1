fa5d9ee98b921029f0e2900d707a3381c9b9af65
Fraidoon Pourooshasb <fpourooshasb@gmail.com>
2023-06-23 19:48:15 -0400

    Initial Commit

---
+++ BillSplit/BillSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitApp.swift
+//  BillSplit
+//
+//  Created by Fraidoon Pourooshasb on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
